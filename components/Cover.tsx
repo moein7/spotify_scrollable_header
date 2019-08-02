4caf860f38a6410e29b367f422d503635f32b1ce
Moein <moein@Moeins-MacBook-Pro.local>
2019-08-02 14:25:25 +0430

    finished

---
+++ components/Cover.tsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+import { Image, StyleSheet, View } from "react-native";
+import { Album, MAX_HEADER_HEIGHT, HEADER_DELTA } from "./Model";
+import Animated from "react-native-reanimated";
+
+interface CoverProps {
+  album: Album;
+  y: Animated.Value<number>
+}
+
+const { interpolate, Extrapolate } = Animated
+
+export default ({ album: { cover }, y}: CoverProps) => {
+  const scale: any = interpolate(y, {
+    inputRange: [-MAX_HEADER_HEIGHT, 0],
+    outputRange: [4, 1],
+    extrapolateRight:Extrapolate.CLAMP
+  });
+  const opacity = interpolate(y, {
+    inputRange: [-62, 0, HEADER_DELTA],
+    outputRange: [0, 0.2, 1],
+    extrapolate:Extrapolate.CLAMP
+  });
+  return (
+    <Animated.View style={[styles.container, { transform: [{ scale }] }]}>
+      <Image style={styles.image} source={cover} />
+      <Animated.View
+        style={{ ...StyleSheet.absoluteFillObject, backgroundColor: "black", opacity }}
+      />
+    </Animated.View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: MAX_HEADER_HEIGHT,
+  },
+  image: {
+    ...StyleSheet.absoluteFillObject,
+    width: undefined,
+    height: undefined,
+  },
+});
